9395181f8d2112e96090a9fa1ebb95445ca07c25
basmaalqadiri <basmanaserqadiri@gmail.com>
2021-02-24 22:57:29 +0200

    functions

---
+++ apps.js
@@ -0,0 +1,10 @@
+first grad =prompt("please enter the first grade"); {
+    else  if (counter==1)
+}
+
+second grad =prompt ("please enter the second grade");{
+
+else
+third grade =prompt (please enter the third grade);{
+
+}
